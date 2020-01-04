d2d9d5faba98b467446b29cd21181f6fd9f386c0
Tim Stahl <timothystahl@gmail.com>
2020-01-04 13:06:51 -0700

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+//Ask for users name
+//add the name to list of names
+//Ask for users party count
+//Add the party count to total
+//print out all names
+//print out total party number
+
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> parties = new List<string>();
+            int totalGuests = 0;
+            
+            string moreGuestsComing = "";
+            do
+            {
+                
+                string partyName = GetInfoFromConsole("What is your party/group name: ");
+
+                parties.Add(partyName);
+
+                totalGuests += GetPartySize();
+
+                moreGuestsComing = GetInfoFromConsole("Do you want to add another guest(yes/no): ");
+
+            } while (moreGuestsComing.ToLower() == "yes");
+
+            Console.WriteLine();
+            Console.WriteLine("Guest Parties at Event:");
+            foreach (string name in parties)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Total Guests: { totalGuests }");
+
+            Console.ReadLine();
+        }
+
+        private static int GetPartySize()
+        {
+            bool isValidNumber = false;
+            int output = 0;
+            do
+            {
+                string partySizeText = GetInfoFromConsole("How big is your party size: ");
+
+                isValidNumber = int.TryParse(partySizeText, out output);
+
+            } while (isValidNumber == false);
+
+            return output;
+        }
+
+        private static string GetInfoFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+    }
+}
